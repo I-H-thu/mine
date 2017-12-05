3faa64c5b11d05f3cd723ba29fed57a53f0a741b
I-H-thu <hiroaki07301@yahoo.co.jp>
2017-12-05 10:20:54 +0900

    Add files via upload

--- renritsu.cpp
+++ renritsu.cpp
@@ -6,11 +6,15 @@ using namespace std;
 #define FOR(i,j,k) for(int i=j;i<k;i++)
 
 const int N = 3;//n元連立方程式
-double a[N][N]={{3.0, 1.0, 2.0},{5.0, 1.0, 3.0},{4.0, 2.0, 1.0}};
-double b[N]={13.0, 20.0, 13.0};
+double a[N][N]={
+                   {3.0, 1.0, 2.0},
+                   {5.0, 1.0, 3.0},
+                   {4.0, 2.0, 1.0}
+                };
+double b[N]={13.0, 20.0, 13.0};//2.1.3
 
 int main (){
-    int sum = 0;
+   double sum = 0.0;
 //前進消去
     REP(k,N-1){
         FOR(i,k+1,N){
